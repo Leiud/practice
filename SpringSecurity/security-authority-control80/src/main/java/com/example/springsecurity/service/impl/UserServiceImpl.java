abbd15cc0ff60af4df0c42626c4449ae95abd804
atideas <lau5464@126.com>
2022-02-25 21:11:44 +0800

    权限控制

--- SpringSecurity/security-authority-control80/src/main/java/com/example/springsecurity/service/impl/UserServiceImpl.java
+++ SpringSecurity/security-authority-control80/src/main/java/com/example/springsecurity/service/impl/UserServiceImpl.java
@@ -9,7 +9,7 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
-// @Service
+@Service
 public class UserServiceImpl implements UserDetailsService {
 
     @Autowired
@@ -18,12 +18,14 @@ public class UserServiceImpl implements UserDetailsService {
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
 
-        // 1. 查询数据库判断用户名是否存在，如果不存在抛出 UsernameNotFoundException 异常
+        // 1.查询数据库判断用户名是否存在，如果不存在抛出 UsernameNotFoundException 异常
         if (!"admin".equals(username)){
             throw new UsernameNotFoundException("用户名不存在");
         }
-        // 2. 把查询出来的密码（注册时已经加密过）进行解析，或直接把密码放入构造方法中
-        String password = passwordEncoder.encode("123");
-        return new User(username,password, AuthorityUtils.commaSeparatedStringToAuthorityList("admin,normal"));
+        // 2.把查询出来的密码（注册时已经加密过）进行解析，或直接把密码放入构造方法中
+        String password = passwordEncoder.encode("1234");
+        // 返回登录用户信息
+        // return new User(username,password, AuthorityUtils.commaSeparatedStringToAuthorityList("admin,normal"));
+        return new User(username,password, AuthorityUtils.commaSeparatedStringToAuthorityList("admin,normal,ROLE_abc"));
     }
 }
