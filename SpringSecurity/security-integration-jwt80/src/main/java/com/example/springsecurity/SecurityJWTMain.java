a57914f4c3d6dca776b6a483d2c12780cee7c907
atideas <lau5464@126.com>
2022-03-01 09:33:51 +0800

    子模块-SpringSecurity整合Oauth2、JWT

---
+++ SpringSecurity/security-integration-jwt80/src/main/java/com/example/springsecurity/SecurityJWTMain.java
@@ -0,0 +1,12 @@
+package com.example.springsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityJWTMain {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecurityJWTMain.class, args);
+    }
+}
---
+++ SpringSecurity/security-integration-jwt80/src/main/java/com/example/springsecurity/service/impl/UserDetailsServiceImpl.java
@@ -0,0 +1,23 @@
+package com.example.springsecurity.service.impl;
+
+import com.example.springsecurity.domain.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        String password = passwordEncoder.encode("123456");
+        return new User("admin", password, AuthorityUtils.commaSeparatedStringToAuthorityList("admin"));
+    }
+}
