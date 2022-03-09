c964298f043b15e8e34060b67012faaf9113e920
风燈 <64816047+atideas@users.noreply.github.com>
2022-03-09 14:33:24 +0800

    Delete SpringSecurity directory

--- SpringSecurity/security-customer-authentication80/src/main/java/com/example/springsecurity/mapper/UserMapper.java
+++
@@ -1,7 +0,0 @@
-package com.example.springsecurity.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.example.springsecurity.domain.User;
-
-public interface UserMapper extends BaseMapper<User> {
-}
